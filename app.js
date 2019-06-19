a12137ca602f5b7568e6c8266f95bca16f7209cd
Poe <piotr.a.ptak@icloud.com>
2019-06-19 19:39:20 +0200

    Install fastify and create the most basic server (it just listens)

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log('hello world');
+const PORT = 4044;
+
+const fastify = require('fastify')({
+  logger: true,
+});
+
+fastify.listen(PORT | 4044, '0.0.0.0', (err, address) => {
+  if (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+  fastify.log.info(`Server listeinng on ${address}`);
+});
