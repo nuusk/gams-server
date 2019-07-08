d6eb2ded27a444f887fbbfce2e501ef31a6d55b8
Poe <piotr.a.ptak@icloud.com>
2019-07-08 22:58:15 +0200

    Fix typo in process variable name

--- helpers/generateToken.js
+++ helpers/generateToken.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 
 const generateToken = (user) => {
   const payload = { sub: user.id };
-  const secret = process.env.SECRET;
+  const secret = process.env.SECRET_KEY;
   return jwt.sign(payload, secret);
 };
 
