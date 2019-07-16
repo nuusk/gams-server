8f5c98a11521c3ff14bd7e00e141dcb8dffb01fe
Poe <piotr.a.ptak@icloud.com>
2019-07-16 11:44:53 +0200

    Make an exception for CORS testing

--- app.js
+++ app.js
@@ -16,8 +16,7 @@ const whitelist = [process.env.CLIENT_ADDRESS];
 
 const corsOptions = {
   origin(origin, callback) {
-    debug(origin);
-    if (whitelist.indexOf(origin) !== -1) {
+    if (whitelist.indexOf(origin) !== -1 || process.env.NODE_ENV === 'test') {
       callback(null, true);
     } else {
       callback(new Error('Not allowed by CORS'));
