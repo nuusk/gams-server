a04c1d856352f4d27dae35c286d37988157a6969
Poe <piotr.a.ptak@icloud.com>
2019-07-18 00:51:25 +0200

    Fix cors options issues with protocols
    - search for domain (which is origin without http:// or https://)
    - cors middleware now uses environmantal variables at it's actually supposed to

--- middleware/cors.js
+++ middleware/cors.js
@@ -1,8 +1,11 @@
+require('dotenv').config();
+
 const whitelist = [process.env.CLIENT_ADDRESS];
 
 const corsOptions = {
   origin(origin, callback) {
-    if (whitelist.indexOf(origin) !== -1 || process.env.NODE_ENV === 'test') {
+    const domain = origin.split('//')[1];
+    if (whitelist.indexOf(domain) !== -1 || process.env.NODE_ENV === 'test') {
       callback(null, true);
     } else {
       callback(new Error('Not allowed by CORS'));
